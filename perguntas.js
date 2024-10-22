7ce17f177146e714750dc196e68ea524e9c067c8
dexterkrieg <161467020+dexterkrieg@users.noreply.github.com>
2024-10-22 10:12:31 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'comida',
+    'O que é um pão?',
+    'é uma mistura de trigo água sal ovos e fermento'
+)
+criaCartao(
+    'vida',
+    'O que é o amor?',
+    'uma reação química que leva os animais a reprodução'
+)
+criaCartao(
+    'Computador',
+    'como copia e cola no PC?',
+    'ctrl+C Copia, ctrl+V cola'
+)
+criaCartao(
+    'hot dog',
+    'quando tempo vive um cachorro?',
+    'depende da raça mas em media 15 a 16 nos'
+)
